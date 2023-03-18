a91445446fa3293c9450c2f6e2e11afa49f020ff
RexGoliath1 <sgoncia@gmail.com>
2023-03-18 03:57:28 -0500

    Working

--- camera-test/ContentView.swift
+++ camera-test/ContentView.swift
@@ -5,9 +5,9 @@
 //  Created by Steven Gonciar on 3/18/23.
 //
 
-import SwiftUI
+//import SwiftUI
 
-struct ContentView: View {
+/*struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -22,5 +22,91 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}*/
+
+import SwiftUI
+import AVFoundation
+
+struct CameraView: View {
+    @StateObject var camera = CameraModel()
+    
+    var body: some View {
+        VStack {
+            if let image = camera.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            } else {
+                Color.black
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .onAppear(perform: {
+            camera.checkPermission()
+            camera.setupSession()
+        })
+    }
+}
+
+class CameraModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    @Published var image: UIImage?
+    
+    private let captureSession = AVCaptureSession()
+    private let videoOutput = AVCaptureVideoDataOutput()
+    private let sessionQueue = DispatchQueue(label: "sessionQueue")
+    
+    func checkPermission() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        switch status {
+        case .authorized:
+            return
+        case .notDetermined:
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if !granted {
+                    fatalError("Camera permission denied")
+                }
+                self.sessionQueue.resume()
+            }
+        default:
+            fatalError("Camera permission denied")
+        }
+    }
+    
+    func setupSession() {
+        sessionQueue.async {
+            guard let device = AVCaptureDevice.default(for: .video) else {
+                fatalError("No camera found")
+            }
+            
+            let input = try! AVCaptureDeviceInput(device: device)
+            if self.captureSession.canAddInput(input) {
+                self.captureSession.addInput(input)
+            }
+            
+            self.videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoOutputQueue"))
+            self.videoOutput.alwaysDiscardsLateVideoFrames = true
+            self.videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            
+            if self.captureSession.canAddOutput(self.videoOutput) {
+                self.captureSession.addOutput(self.videoOutput)
+            }
+            
+            self.captureSession.startRunning()
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let ciImage = CIImage(cvImageBuffer: pixelBuffer)
+        let context = CIContext()
+        let cgImage = context.createCGImage(ciImage, from: ciImage.extent)
+        
+        DispatchQueue.main.async {
+            self.image = UIImage(cgImage: cgImage!)
+        }
     }
 }
--- camera-test/camera_testApp.swift
+++ camera-test/camera_testApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct camera_testApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CameraView()
         }
     }
 }
