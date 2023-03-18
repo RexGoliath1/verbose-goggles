ac08ce4b5442001628871a71221a02d955614994
RexGoliath1 <sgoncia@gmail.com>
2023-03-18 03:07:42 -0500

    Initial Commit

---
+++ camera-test/camera_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_testApp.swift
+//  camera-test
+//
+//  Created by Steven Gonciar on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct camera_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ camera-testUITests/camera_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  camera_testUITestsLaunchTests.swift
+//  camera-testUITests
+//
+//  Created by Steven Gonciar on 3/18/23.
+//
+
+import XCTest
+
+final class camera_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
